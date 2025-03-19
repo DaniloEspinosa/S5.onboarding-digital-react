7cebd9db80cd732e521ad53a88f4c3354a7d2475
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-03-19 20:42:01 +0100

    ejercicio 3 nivel 1

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,38 @@ const tutorialData = [
     title: "Dedica moltes hores",
     description:
       "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora rapidament.",
-    bgColor: "cyan",
-    image: "",
+    bgColor: "#4da2a9",
+    image: "/time_managment.svg",
   },
   {
     title: "Programa projectes propis",
     description:
       "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-    bgColor: "gray",
-    image: "",
+    bgColor: "#d3d4d9",
+    image: "/programming.svg",
   },
   {
     title: "Procura descansar",
     description:
       "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-    bgColor: "yellow",
-    image: "",
+    bgColor: "#ffd167",
+    image: "/meditation.svg",
   },
 ];
 
 function App() {
   const [step, setStep] = useState(0);
 
-  const currentCardData = tutorialData[step]
+  const currentCardData = tutorialData[step];
 
   const nextStep = () => {
-    setStep(step + 1)
-  }
+    if (step < tutorialData.length) setStep(step + 1);
+    if (step == tutorialData.length - 1 ) setStep(0)
+  };
 
   return (
     <>
-      <Card {...currentCardData} nextStep={nextStep}/>
+      <Card {...currentCardData} nextStep={nextStep} step={step} />
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,41 @@
 interface CardProps {
-    title: string;
-    description: string;
-    bgColor: string;
-    image: string;
-    nextStep: () => void
+  title: string;
+  description: string;
+  bgColor: string;
+  image: string;
+  nextStep: () => void;
+  step: number;
 }
 
-const Card = ({title, description, nextStep}: CardProps) => {
+const Card = ({
+  title,
+  description,
+  image,
+  bgColor,
+  step,
+  nextStep,
+}: CardProps) => {
   return (
-    <>
-    <h2>{title}</h2>
-    <p>{description}</p>
-    <button onClick={nextStep}>Next</button>
-    </>
-  )
-}
+    <div className="card">
+      <div className="card-header" style={{ backgroundColor: bgColor }}>
+        <img src={image} alt={title} />
+      </div>
 
-export default Card
+      <div className="card-body">
+        <h2>{title}</h2>
+        <p>{description}</p>
+
+        <div className="card-buttons">
+          <div className="selector-card">
+            <p className={step == 0 ? "card-active" : "card-inactive"}></p>
+            <p className={step == 1 ? "card-active" : "card-inactive"}></p>
+            <p className={step == 2 ? "card-active" : "card-inactive"}></p>
+          </div>
+          <button onClick={nextStep}>➔</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
